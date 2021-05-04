8caa00484677eaef2aaab0bfda8a4258a6672a79
Lakshmi Narayanan <lnarayanan47@gmail.com>
2021-05-04 17:40:30 +0530

    Add files via upload

---
+++ lib/providers/core_provider.dart
@@ -0,0 +1,123 @@
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:filex/utils/utils.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:isolate_handler/isolate_handler.dart';
+import 'package:path_provider/path_provider.dart';
+
+class CoreProvider extends ChangeNotifier {
+  List<FileSystemEntity> availableStorage = <FileSystemEntity>[];
+  List<FileSystemEntity> recentFiles = <FileSystemEntity>[];
+  final isolates = IsolateHandler();
+  int totalSpace = 0;
+  int freeSpace = 0;
+  int totalSDSpace = 0;
+  int freeSDSpace = 0;
+  int usedSpace = 0;
+  int usedSDSpace = 0;
+  bool storageLoading = true;
+  bool recentLoading = true;
+
+  checkSpace() async {
+    setRecentLoading(true);
+    setStorageLoading(true);
+    recentFiles.clear();
+    availableStorage.clear();
+    List<FileSystemEntity> l = await getExternalStorageDirectories();
+    availableStorage.addAll(l);
+    notifyListeners();
+    MethodChannel platform = MethodChannel('dev.jideguru.filex/storage');
+    var free = await platform.invokeMethod("getStorageFreeSpace");
+    var total = await platform.invokeMethod("getStorageTotalSpace");
+    setFreeSpace(free);
+    setTotalSpace(total);
+    setUsedSpace(total - free);
+    if (l.length > 1) {
+      var freeSD = await platform.invokeMethod("getExternalStorageFreeSpace");
+      var totalSD = await platform.invokeMethod("getExternalStorageTotalSpace");
+      setFreeSDSpace(freeSD);
+      setTotalSDSpace(totalSD);
+      setUsedSDSpace(totalSD - freeSD);
+    }
+    setStorageLoading(false);
+    getRecentFiles();
+  }
+
+  getRecentFiles() async {
+    String isolateName = 'recent';
+    isolates.spawn<String>(
+      getFilesWithIsolate,
+      name: isolateName,
+      onReceive: (val) {
+        print(val);
+        isolates.kill(isolateName);
+      },
+      onInitialized: () => isolates.send('hey', to: isolateName),
+    );
+    ReceivePort _port = ReceivePort();
+    IsolateNameServer.registerPortWithName(_port.sendPort, '${isolateName}_2');
+    _port.listen((message) {
+      print('RECEIVED SERVER PORT');
+      print(message);
+      recentFiles.addAll(message);
+      setRecentLoading(false);
+      _port.close();
+      IsolateNameServer.removePortNameMapping('${isolateName}_2');
+    });
+  }
+
+  static getFilesWithIsolate(Map<String, dynamic> context) async {
+    print(context);
+    String isolateName = context['name'];
+    List<FileSystemEntity> l =
+        await FileUtils.getRecentFiles(showHidden: false);
+    final messenger = HandledIsolate.initialize(context);
+    final SendPort send =
+        IsolateNameServer.lookupPortByName('${isolateName}_2');
+    send.send(l);
+    messenger.send('done');
+  }
+
+  void setFreeSpace(value) {
+    freeSpace = value;
+    notifyListeners();
+  }
+
+  void setTotalSpace(value) {
+    totalSpace = value;
+    notifyListeners();
+  }
+
+  void setUsedSpace(value) {
+    usedSpace = value;
+    notifyListeners();
+  }
+
+  void setFreeSDSpace(value) {
+    freeSDSpace = value;
+    notifyListeners();
+  }
+
+  void setTotalSDSpace(value) {
+    totalSDSpace = value;
+    notifyListeners();
+  }
+
+  void setUsedSDSpace(value) {
+    usedSDSpace = value;
+    notifyListeners();
+  }
+
+  void setStorageLoading(value) {
+    storageLoading = value;
+    notifyListeners();
+  }
+
+  void setRecentLoading(value) {
+    recentLoading = value;
+    notifyListeners();
+  }
+}
